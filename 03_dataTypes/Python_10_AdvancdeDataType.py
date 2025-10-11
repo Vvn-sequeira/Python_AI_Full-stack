232501a9c2347128792bcf8e5449b8ba4dc7bf93
vvn-sequeira <svivianmarcel@gmail.com>
2025-10-11 22:31:22 +0530

    dictionary Datatypes Advaced DT

---
+++ 03_dataTypes/Python_10_AdvancdeDataType.py
@@ -0,0 +1,4 @@
+import arrow ;
+
+UTC_Time =  arrow.utcnow()
+UTC_Time.to("Europe/Rome")
--- 03_dataTypes/chapter_9_Dictionary.py
+++ 03_dataTypes/chapter_9_Dictionary.py
@@ -19,10 +19,30 @@ del Student['college']
 # membership
 print(f"Is Sugar in the Order ? {'name' in Student}")
 
-
 print(f"the keys are : {Student.keys()}")
 print(f"the values are : {Student.values()}")
 
+# Pop the ele for the dict
+last_item = Human.popitem()
+print(f"last item of humna DIct is : {last_item}")
+print(f"after Poped the ele  : {Human}")
+
+# Update the ele for the dict 
+updated_Human = {"name" :"Marcel" , "country" : "India" , "State" : "Delhi"}
+Human.update(updated_Human);
+print(f"Human : {Human}");
+
+# CityofHuman = Human["City"]
+# print(f"City of human {CityofHuman}")
+
+StateOfHuman = Human.get("City" , "No info of State ");
+print(f"State of Human {StateOfHuman}")
 
 
+# Sets Operation 
+#Human = dict(name="vivian" , country="India" , State="Karnataka")
+# Student["name"] = "vivian"
+# Student["city"] = "Hassan" 
 
+UnionDict = Human | Student
+print(f"Uniion of two Data set {UnionDict}")
