ff4c31dbfc448cb7df8576691d428620f143893f
vvn-sequeira <svivianmarcel@gmail.com>
2026-01-27 00:30:46 +0530

    Agent is working

--- Weather_agent/req.py
+++ Weather_agent/req.py
@@ -1,7 +1,29 @@
 import requests
+from langchain_ollama import ChatOllama
+import json 
 
-URL = "https://wttr.in/{Mangalore}?format=%C+%t"
+# URL = "https://wttr.in/Delhi?format=%C+%t"
 
-res = requests.get(URL)
+# res = requests.get(URL)
 
-print(res.status_code , "\n" , res.text)
+# print(res.status_code , "\n" , res.text) 
+
+def printf ():
+    print("hello world !")
+
+available_tools = {
+    'printTool' : printf 
+}
+
+available_tools['printTool']() 
+
+
+# llm = ChatOllama(
+#     model="qwen2.5:7b-instruct",
+#     base_url="http://localhost:11434",
+# )
+
+# res = llm.invoke("Respond only in JSON: {\"hello\": \"world\"}")
+# print(res.content)
+# jsonres = json.loads(res.content)
+# print(type(jsonres))
